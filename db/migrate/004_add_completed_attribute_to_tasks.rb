e545418768f8eb7c7a6b12755188a8cd6498071a
Chris Williams <chris.d.williams@gmail.com>
2008-02-24 22:33:03 -0500

    Add the migration I forgot to add.

---
+++ db/migrate/004_add_completed_attribute_to_tasks.rb
@@ -0,0 +1,9 @@
+class AddCompletedAttributeToTasks < ActiveRecord::Migration
+  def self.up
+    add_column :tasks, :completed, :boolean, :default => false
+  end
+
+  def self.down
+    remove_column :tasks, :completed
+  end
+end
